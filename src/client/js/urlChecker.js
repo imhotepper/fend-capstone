78b6b6013032fcb76bb0290a1a1723ab9854943e
dragosh <stefanescu.dragos@gmail.com>
2020-05-25 10:33:18 +0300

    some tests on the fe side

--- src/client/js/urlChecker.js
+++
@@ -1,7 +0,0 @@
-function urlChecker(possibleUrl) {
-    var urlPattern = /^(https?|ftp|torrent|image|irc):\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$/i;
-    var res = possibleUrl.match(urlPattern);
-    return (res !== null)
-}
-
-export { urlChecker }
